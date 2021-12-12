5205b0b5a5b5a17195ba915409b182bbd3171109
Alexandr Chikur <alexandr.chikur@mail.ru>
2021-12-13 02:50:07 +0300

    Change constants vue cons name

--- src/plugins/Constants.js
+++ src/plugins/Constants.js
@@ -1,8 +1,9 @@
 let Constants = {
     install(Vue) {
-      Vue.Constants = {
+      Vue.prototype.$constants = {
         VERSION: "1.0.0",
-        DEBUG: true
+        DEBUG: true,
+        POINT_NAME_LENGTH: 14,
       };
     }
   };
