0f5d7e07437c2710afd62d40f0a12180d2a74e1f
Alexandr Chikur <alexandr.chikur@mail.ru>
2021-12-13 02:29:08 +0300

    Add plugin with Constants

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Constants from './plugins/Constants'
+
 Vue.config.productionTip = false
 
+Vue.use(Constants)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
---
+++ src/plugins/Constants.js
@@ -0,0 +1,10 @@
+let Constants = {
+    install(Vue) {
+      Vue.Constants = {
+        VERSION: "1.0.0",
+        DEBUG: true
+      };
+    }
+  };
+  
+  export default Constants;
