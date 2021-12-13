0688e585fd8d0d522f64efe939c6ab864072cf1d
Alexandr Chikur <alexandr.chikur@mail.ru>
2021-12-13 23:29:57 +0300

    Complete Header component

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,20 @@
 <template>
     <div class="root_header">
         <div class="header">
-
+            <div class="header__title">
+                <div class="header__title_name">{{ this.$constants.TITLE }}</div>
+                <div class="header__title_version">v.{{ this.$constants.VERSION }}</div>
+            </div>
+            <div class="header__user" v-if="temp.loggedIn">
+                <div class="header__user_name">{{ temp.uName }}</div>
+                <img src="../assets/img/logout_icon.svg" class="header__user__logout_icon">
+            </div>
+            <div class="header__auth" v-else>
+                <ul>
+                    <li><a href="#">SIGN IN</a></li>
+                    <li><a href="#">SIGN UP</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -11,12 +24,15 @@ export default {
     name: 'HeaderComponent',
     data() {
         return {
-
+            temp: { // temporary object
+                uName: "Nickey L.",
+                loggedIn: false,
+            }
         }
     },
     methods: {
 
-    }
+    },
 }
 </script>
 
--- src/plugins/Constants.js
+++ src/plugins/Constants.js
@@ -1,6 +1,7 @@
 let Constants = {
     install(Vue) {
       Vue.prototype.$constants = {
+        TITLE: "Mapoints",
         VERSION: "1.0.0",
         DEBUG: true
       };
