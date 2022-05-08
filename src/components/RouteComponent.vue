7faa5354360c53c1da478d68d2ee7bdaef465836
Alexandr Chikur <alexandr.chikur@mail.ru>
2022-05-08 03:40:45 +0300

    Add RouteComponent empty template

---
+++ src/components/RouteComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="root_route">
+        <div class="route">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+import PointComponent from './PointComponent.vue'
+export default {
+  components: { PointComponent },
+    name: 'PointsComponent',
+    data() {
+        return {
+            temp: { // temporary value
+                pointsExists: true,
+                pointTitle: "Lorem ipsum dolor sot"
+            }
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<style scoped>
+
+
+</style>
