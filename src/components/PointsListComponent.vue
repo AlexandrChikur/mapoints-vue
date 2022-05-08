5070aaf0129a8b5638225777bf1fee9856f55799
Alexandr Chikur <alexandr.chikur@mail.ru>
2022-05-08 03:15:01 +0300

    Add PointsListComponent features

--- src/components/PointsListComponent.vue
+++ src/components/PointsListComponent.vue
@@ -4,10 +4,10 @@
             <div class="points-list__header">
                 <div class="points-list__header_title">My points</div>
                 <div class="points__header__btns flex_nowrap cp">
-                    <div class="points__header__btns__interact flex_nowrap">
+                    <!-- <div class="points__header__btns__interact flex_nowrap">
                         <div class="points__header__btns__interact_text points__header__btn_txt">Interact</div>
                         <img src="../assets/img/ico/play.svg" alt="" class="points__header__btns_interact-icon points__header__btn_ico">
-                    </div>
+                    </div> -->
                     <div class="points__header__btns__create flex_nowrap cp">
                         <div class="points__header__btns__interact_text points__header__btn_txt">Create a point</div>
                         <img src="../assets/img/ico/add.svg" alt="" class="points__header__btns_add-icon points__header__btn_ico">
