eaf800b1303690648359b8537afc8cc1d4103bd9
Alexandr Chikur <alexandr.chikur@mail.ru>
2022-05-08 08:04:32 +0300

    Add notifications

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import NotifyPlugin from 'vue-easy-notify'
+import 'vue-easy-notify/dist/vue-easy-notify.css' // import style
 // import store from './store/index'
 
 import '../src/assets/styles/main.css'
@@ -9,6 +11,8 @@ import Constants from './plugins/Constants'
 Vue.config.productionTip = false
 
 Vue.use(Constants)
+Vue.use(NotifyPlugin)
+
 new Vue({
   render: h => h(App),
   // store
