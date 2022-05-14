d1d6a8a6fe9cfd64ebaf3f462ca2e6518079d14b
Alexandr Chikur <alexandr.chikur@mail.ru>
2022-05-14 14:23:45 +0300

    Add point chain component

---
+++ src/components/PointsChainComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class = 'points-chain-root'>
+    <div class="points-chain_block">
+        <div class="points-chain_block__help">Shift + MButton to scroll</div>
+        <div class="points-chain_block__list">
+            <div class="points-chain_block__node" v-for="(point, idx) in pointList" :key="idx">
+                <point-component 
+                    :pointId="point.id"
+                    :pointName="point.name" 
+                    :authorId="point.user_id"
+                    :authorName="point.author_name"
+                    :coordX="point.x" 
+                    :coordY="point.y"
+                />
+            </div>
+        </div>
+        <div class="points-chain_block__total"> 
+            {{ totalDist }} c.u.
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+import PointComponent from './PointComponent.vue'
+export default {
+  components: { PointComponent },
+    name: 'PointsChainComponent',
+    props: ["pointList", "totalDist"],
+    data() {
+        return {}
+    },
+    mounted() {},
+}
+</script>
+
+<style>
+
+</style>
