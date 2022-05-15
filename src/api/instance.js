4a3124db213e50a14a26ebb05172b5d16fe304c0
Alexandr Chikur <alexandr.chikur@mail.ru>
2022-05-15 12:44:39 +0300

    Build for heroku

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "http://localhost:8000/api/",
+    // baseURL: "http://localhost:8000/api/",
+    baseURL: "https://mapoints-api.herokuapp.com/api/",
     withCredentials: true,
     headers: {
         accept: "application/json"
